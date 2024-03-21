bcb98bba456b856e33b607754800cf226ac0a2dc
loubing <loubing@lovol.com>
2024-03-21 09:48:06 +0800

    【路由】gradle文件配置

---
+++ app/src/main/java/com/example/router/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.router
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.router.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
